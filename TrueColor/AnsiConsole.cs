c60b5042897a53c17c463907f78e764483a15c90
HexMerlin <hexmerlin@pm.me>
2025-08-12 15:00:54 +0200

    feat: move Rgb struct to its own file
    The `Rgb` struct has been relocated from `AnsiConsole.cs` to a new file, `Rgb.cs`. This change enhances code organization and maintains the struct's functionality, including its properties and methods for equality comparison and hash code generation.

--- TrueColor/AnsiConsole.cs
+++ TrueColor/AnsiConsole.cs
@@ -3,22 +3,6 @@ using System.Runtime.InteropServices;
 
 namespace TrueColor;
 
-public readonly struct Rgb : IEquatable<Rgb>
-{
-    public byte R { get; }
-    public byte G { get; }
-    public byte B { get; }
-
-    public Rgb(byte r, byte g, byte b) => (R, G, B) = (r, g, b);
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public bool Equals(Rgb other) => R == other.R && G == other.G && B == other.B;
-    public override bool Equals(object? obj) => obj is Rgb o && Equals(o);
-    public override int GetHashCode() => (R << 16) | (G << 8) | B;
-    public static bool operator ==(Rgb a, Rgb b) => a.Equals(b);
-    public static bool operator !=(Rgb a, Rgb b) => !a.Equals(b);
-}
-
 public static class AnsiConsole
 {
     private static readonly Stream Stdout = Console.OpenStandardOutput();
