fecd50e4d2553ef5e861d65cfa1ee1f96d1f367f
HexMerlin <hexmerlin@pm.me>
2025-08-23 17:12:18 +0200

    feat: update namespace and enable demo execution
    Changed namespace to `SemanticTokens.DevConsole` in `Program.cs`.
    Uncommented and executed demo instances for `ConsoleDemo` and `CSharpSyntaxHighlightingDemo`.

--- SemanticTokens.DevConsole/Program.cs
+++ SemanticTokens.DevConsole/Program.cs
@@ -9,15 +9,15 @@ namespace SemanticTokens.DevConsole;
 internal static class Program
 {
     private static async Task Main()
-    {     
+    {
         //Console.Title = "Showing some Console Demos";
 
         //comment out to run specific demo
-        //ConsoleDemo demo1 = new ConsoleDemo();
-        //demo1.Run();
+        ConsoleDemo demo1 = new ConsoleDemo();
+        demo1.Run();
 
-        //CSharpSyntaxHighlightingDemo demo2 = new CSharpSyntaxHighlightingDemo();
-        //await demo2.Run();
+        CSharpSyntaxHighlightingDemo demo2 = new CSharpSyntaxHighlightingDemo();
+        await demo2.Run();
 
         ConsoleImageDemo demo3 = new ConsoleImageDemo();
         demo3.Run(); 
