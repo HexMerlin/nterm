801ba328394b5f85ad198c2af3f4656e9255c248
HexMerlin <hexmerlin@pm.me>
2025-08-27 22:00:17 +0200

    feat: update image encoding logic and cleanup
    Refactor `CurEncoder` and `IcoEncoder` to remove size checks for images larger than 256x256 pixels, replacing them with comments about frame selection logic. Update method calls to use `SixelEncode`.
    
    Enhance `SixelDecode` by increasing initial canvas size to 800x600 and improving resizing logic to track maximum dimensions during parsing. Clean up `Dispose` method in `SixelEncoder` by removing unnecessary comments.

--- SemanticTokens.Sixel/Encoder/SixelEncoder.cs
+++ SemanticTokens.Sixel/Encoder/SixelEncoder.cs
@@ -346,18 +346,12 @@ public class SixelEncoder(Image<Rgba32> img, string? format) : IDisposable
                 Image.Dispose();
             }
 
-            // TODO: free unmanaged resources (unmanaged objects) and override finalizer
-            // TODO: set large fields to null
+
             disposedValue = true;
         }
     }
 
-    // // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
-    // ~Encoder()
-    // {
-    //     // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
-    //     Dispose(disposing: false);
-    // }
+
 
     public void Dispose()
     {
